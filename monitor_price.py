84b71cb85d7c737f209c6ea1a375a3351db6a835
TheCracker007 <69054068+TheCracker007@users.noreply.github.com>
2024-01-11 18:54:52 +0530

    Update monitor_price.py

--- monitor_price.py
+++ monitor_price.py
@@ -1,15 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-import time
 
 def get_latest_sell_order():
     url = "https://www.binance.com/en/orderbook/USDT_VAI"
     response = requests.get(url)
-    
+
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         sell_orders = soup.select('#sellOrders tbody tr')
-        
+
         if sell_orders:
             latest_sell_order = sell_orders[0]
             price_vai = latest_sell_order.find('td', class_='price').get_text(strip=True)
@@ -23,18 +22,12 @@ def update_readme(price_vai, amount_usdt):
         readme_file.write(f"**Price (VAI)**: {price_vai}\n")
         readme_file.write(f"**Amount (USDT)**: {amount_usdt}\n")
 
-def main():
-    while True:
-        price_vai, amount_usdt = get_latest_sell_order()
-        
-        if price_vai is not None and amount_usdt is not None:
-            print("Latest Sell Order:")
-            print(f"Price (VAI): {price_vai}")
-            print(f"Amount (USDT): {amount_usdt}\n")
-
-            update_readme(price_vai, amount_usdt)
-        
-        time.sleep(300)  # Sleep for 5 minutes
-
 if __name__ == "__main__":
-    main()
+    price_vai, amount_usdt = get_latest_sell_order()
+
+    if price_vai is not None and amount_usdt is not None:
+        print("Latest Sell Order:")
+        print(f"Price (VAI): {price_vai}")
+        print(f"Amount (USDT): {amount_usdt}\n")
+
+        update_readme(price_vai, amount_usdt)
